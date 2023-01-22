da8cb36bd84f9b2d2f14d091454a25d8b5f009e6
Eugene Tokarev <eugene@gmail.com>
2023-01-22 16:29:39 -0600

    implement basic REST service

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,67 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    http::StatusCode,
+    response::IntoResponse,
+    routing::{get, post},
+    Json, Router,
+};
+use serde::{Deserialize, Serialize};
+use std::net::SocketAddr;
+use tracing::{info, error, Level};
+use tracing_subscriber::FmtSubscriber;
+
+#[tokio::main]
+async fn main() {
+    // initialize tracing
+    let subscriber = FmtSubscriber::builder()
+        .with_max_level(Level::TRACE)
+        .finish();
+    tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
+
+
+    // build our application with a route
+    let app = Router::new()
+        // `POST /users` goes to `create_user`
+        .route("/currency", post(convert_currency));
+
+    // run our app with hyper
+    // `axum::Server` is a re-export of `hyper::Server`
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    info!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn convert_currency(
+    // this argument tells axum to parse the request body
+    // as JSON into a `CreateUser` type
+    Json(payload): Json<ConvertCurrency>,
+) -> impl IntoResponse {
+    // insert your application logic here
+    let response = ConvertResult {
+        from: payload.from,
+        to: payload.to,
+        amount: payload.amount,
+    };
+
+    // this will be converted into a JSON response
+    // with a status code of `201 Created`
+    (StatusCode::OK, Json(response))
+}
+
+// the input to our `create_user` handler
+#[derive(Deserialize)]
+struct ConvertCurrency {
+    to: String,
+    from: String,
+    amount: f32
+}
+
+// the output to our `create_user` handler
+#[derive(Serialize)]
+struct ConvertResult {
+    from: String,
+    to: String,
+    amount: f32
 }

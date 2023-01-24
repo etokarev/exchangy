ffa2573e9d6a26a7835f35ebbee03d652aed317d
Eugene Tokarev <eugene@gmail.com>
2023-01-24 17:40:09 -0600

    make the convert handler work

--- src/main.rs
+++ src/main.rs
@@ -13,8 +13,8 @@ use std::net::SocketAddr;
 use tracing::{info, error, Level, debug};
 use tracing_subscriber::FmtSubscriber;
 use std::collections::HashMap;
-
-
+use std::fmt;
+use std::error::Error;
 // A simple type alias so as to DRY.
 // type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
 
@@ -50,64 +50,54 @@ async fn convert_currency(
     let from = payload.from;
     let to = payload.to;
 
-//   try_thing()?;
-     let from_currency = fetch_country(&from).await?;
-     info!(from_currency="{from_currency}");
+    let from_currency = fetch_country(&from).await?;
+    let to_currency = fetch_country(&to).await?;
+    info!("from_currency={from_currency}, to_currency={to_currency}");
 
-    // insert your application logic here
+    let converted_amount = convert(from_currency, to_currency, &payload.amount).await?;
+
     let response = ConvertResult {
         from: from,
         to: to,
-        amount: payload.amount,
+        amount: converted_amount,
     };
 
     Ok(response.into())
 }
-//-> impl IntoResponse {
-//
-//     let from = payload.from;
-//     let to = payload.to;
-//
-//     let from_currency = fetch_country(from).await?;
-//     info!(from_currency="{from_currency}");
-//
-//     // insert your application logic here
-//     let response = ConvertResult {
-//         from: from,
-//         to: to,
-//         amount: payload.amount,
-//     };
-//
-//     // this will be converted into a JSON response
-//     // with a status code of `201 Created`
-//     (StatusCode::OK, Json(response))
-// }
 
-// Make our own error that wraps `anyhow::Error`.
-struct AppError(anyhow::Error);
+#[derive(Debug)]
+struct AppError{
+    details: String
+}
+
+impl AppError {
+    fn new(msg: &str) -> AppError {
+        AppError{details: msg.to_string()}
+    }
+}
+
+impl fmt::Display for AppError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f,"{}",self.details)
+    }
+}
+
+impl Error for AppError {
+    fn description(&self) -> &str {
+        &self.details
+    }
+}
+
 
 // Tell axum how to convert `AppError` into a response.
 impl IntoResponse for AppError {
     fn into_response(self) -> Response {
         (
             StatusCode::INTERNAL_SERVER_ERROR,
-            format!("Something went wrong: {}", self.0),
-        )
-            .into_response()
+            format!("Something went wrong: {}", self.details),
+        ).into_response()
     }
 }
-
-// This enables using `?` on functions that return `Result<_, anyhow::Error>` to turn them into
-// `Result<_, AppError>`. That way you don't need to do that manually.
-impl<E> From<E> for AppError
-    where
-        E: Into<anyhow::Error>,
-{
-    fn from(err: E) -> Self {
-        Self(err.into())
-    }
-}
-
 
 // the input to our `create_user` handler
 #[derive(Deserialize)]
@@ -125,7 +115,7 @@ struct ConvertResult {
     amount: f32
 }
 
-async fn fetch_country(name: &String) -> Result<String, anyhow::Error> {
+async fn fetch_country(name: &String) -> Result<String, AppError> {
     let url: Uri = format!("https://restcountries.com/v3.1/name/{name}?fields=name,currencies").parse().unwrap();
     info!("fetch_country called with name={name}");
 
@@ -133,18 +123,60 @@ async fn fetch_country(name: &String) -> Result<String, anyhow::Error> {
     let https = HttpsConnector::new();
     let client = Client::builder().build::<_, hyper::Body>(https);
     // Fetch the url...
-    let res = client.get(url).await?;
+    let res = client.get(url).
+        await.map_err(|_| AppError::new("http response error"))?;
+
+    if res.status() != StatusCode::OK {
+        return Err(AppError::new(&format!("bad country name: {name}").to_string()))
+    }
 
     // asynchronously aggregate the chunks of the body
-    let body = hyper::body::aggregate(res).await?;
+    let body = hyper::body::aggregate(res).
+        await.map_err(|_| AppError::new("reading buffer error"))?;
 
     // try to parse as json with serde_json
-    let counties: Vec<Country> = serde_json::from_reader(body.reader())?;
+    let counties: Vec<Country> = serde_json::from_reader(body.reader()).
+        map_err(|_| AppError::new("json deserialization error"))?;
     info!("fetched result with {} items", counties.len());
 
-    Ok("all good".to_string())
+    match counties.len() {
+        1 => {
+            match counties[0].currencies.keys().next() {
+                Some(key) => Ok(key.to_string()),
+                None => Err(AppError::new(&format!("no currency for country {name}").to_string()))
+            }
+        }
+        0 => Err(AppError::new(&format!("could not fetch country {name}").to_string())),
+        _ => Err(AppError::new("unexpected result when fetching country {name}"))
+    }
 }
 
+async fn convert(from: String, to: String, amount: &f32) -> Result<f32, AppError> {
+    let url: Uri = format!("https://v6.exchangerate-api.com/v6/615900b74ad7d5dec68b5f0f/pair/{from}/{to}/{amount}").parse().unwrap();
+    debug!("convert url={}", url.to_string());
+
+
+    let https = HttpsConnector::new();
+    let client = Client::builder().build::<_, hyper::Body>(https);
+    // Fetch the url...
+    let res = client.get(url).
+        await.map_err(|_| AppError::new("http response error"))?;
+
+    if res.status() != StatusCode::OK {
+        return Err(AppError::new(&format!("Exchangerate-api returned {} http code", res.status())))
+    }
+
+    let body = hyper::body::aggregate(res).
+        await.map_err(|_| AppError::new("reading buffer error"))?;
+
+    // try to parse as json with serde_json
+    let result: ExchangeResult = serde_json::from_reader(body.reader()).
+        map_err(|_| AppError::new("json deserialization error"))?;
+    debug!("fetched result {}", result.conversion_result);
+
+
+    Ok(result.conversion_result)
+}
 
 #[derive(Deserialize, Debug)]
 struct Country {
@@ -161,4 +193,13 @@ struct Name {
 struct Currency {
     name: String,
     symbol: String
+}
+
+
+#[derive(Deserialize, Debug)]
+struct ExchangeResult {
+    base_code: String,
+    target_code: String,
+    conversion_rate: f32,
+    conversion_result: f32
 }
